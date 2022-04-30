52e0cbc7e6f3c2ec2447c8c6459f5bc6963c7373
Ebrahim Al-Shargabi <ovt9lc@inf.elte.hu>
2022-04-30 04:26:09 +0200

    Adding the slider class

--- sorting/buttons.py
+++ sorting/buttons.py
@@ -45,3 +45,41 @@ class SizeField:
             if int(self.size) < self.max_size:
                 if event.unicode.isdigit():
                     self.size += event.unicode
+
+
+class Slider:
+    def __init__(self, rect):
+        self.hover = False
+        self.rect = rect
+        self.speed = self.rect.x + line_thickness
+
+    def draw(self):
+        """Draw the slider"""
+        screen.blit(baseFont.render("Speed", True, colors['BLACK']),
+                    (self.rect.x + self.rect.w // 2, self.rect.y - self.rect.h // 2))
+        self.rect.w += 1
+        pygame.draw.rect(screen, colors['BLACK'], self.rect, line_thickness)
+        self.rect.w -= 1
+        pygame.draw.line(screen, colors['BLACK'], (self.rect.x + line_thickness, self.rect.y + self.rect.h // 2),
+                         (self.rect.x + self.rect.w - line_thickness, self.rect.y + self.rect.h // 2),
+                         line_thickness // 2)
+        pygame.draw.line(screen, colors['BLACK'], (self.speed, self.rect.y + line_thickness),
+                         (self.speed, self.rect.y + self.rect.h - line_thickness), line_thickness * 2)
+
+    def update(self, event):
+        """Updating the slider if the user moves it, and changes the speed according to the change"""
+        self.mousePos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(pygame.mouse.get_pos()) == 1:
+            self.hover = True
+        else:
+            self.hover = False
+        initial_speed = self.rect.x + line_thickness
+        self.min_speed = self.rect.x + line_thickness
+        self.max_speed = self.rect.x + self.rect.w - line_thickness
+        self.speed += self.min_speed - initial_speed
+
+        if self.hover:
+            if pygame.mouse.get_pressed() != (0, 0, 0):
+                if (self.rect.x + line_thickness) <= self.mousePos[0] and self.mousePos[0] <= (
+                        self.rect.x + self.rect.w - line_thickness):
+                    self.speed = self.mousePos[0]
