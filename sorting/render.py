2807975df865465a3a2919bdde0367449f55b2f9
Ebrahim Al-Shargabi <ovt9lc@inf.elte.hu>
2022-06-10 07:37:07 +0200

    adding the rendering methods

---
+++ sorting/render.py
@@ -0,0 +1,51 @@
+from buttons import *
+
+
+def renderBottomMenu():
+    """"Draw the menu"""
+    sizeBox.draw()
+    delayBox.draw()
+    algorithmBox.draw()
+    if sorting:
+        stopButton.draw()
+    else:
+        playButton.draw()
+
+
+def updateWidgets(event):
+    """"Update the menu"""
+    sizeBox.update(event)
+    delayBox.update(event)
+    algorithmBox.update()
+    if sorting:
+        stopButton.update()
+    else:
+        playButton.update()
+
+
+def drawBars(array, PURPLE_1, PURPLE_2, TEAL_1, TEAL_2, sorted={}):
+    """Draw the bars and control their colors"""
+
+    if numBars != 0:
+        bar_width = 900 / numBars
+        ceil_width = ceil(bar_width)
+
+        for num in range(numBars):
+            if num in (PURPLE_1, PURPLE_2):
+                color = colors['PURPLE']
+            elif num in (TEAL_1, TEAL_2):
+                color = colors['TEAL']
+            elif num in sorted:
+                color = colors['YELLOW']
+                # pass
+            else:
+                color = colors["BLUE"]
+            pygame.draw.rect(screen, color, (num * bar_width, algorithmBox.rect.y//1.1 - array[num], ceil_width, array[num]))
+
+def render(array, PURPLE_1, PURPLE_2, TEAL_1, TEAL_2, **kwargs):
+    '''Draw all the interface'''
+    screen.fill(colors["WHITE"])
+    drawBars(array, PURPLE_1, PURPLE_2, TEAL_1, TEAL_2, **kwargs)
+    renderBottomMenu()
+    pygame.display.update()
+
